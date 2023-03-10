d61a024aea12fde65c0a5f6b042b5528a95aa731
Luka Keseric <lk6760@student.uni-lj.si>
2023-03-10 15:45:11 +0000

    new hello and test files

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,24 @@
-from hello import add
+from hello import toyou, add, subtract
 
-def test_add():
-    assert add(1,2) == 3
+
+def setup_function(function):
+    print(f" Running Setup: {function.__name__}")
+    function.x = 10
+
+
+def teardown_function(function):
+    print(f" Running Teardown: {function.__name__}")
+    del function.x
+
+
+### Run to see failed test
+def test_hello_add():
+    assert add(test_hello_add.x) == 11
+
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
+
+
+def test_hello_toyou():
+    assert toyou(test_hello_toyou.x) == "hi 10"
